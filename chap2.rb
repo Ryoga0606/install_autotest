1e4897e186e68fb4bed4926ad4e7111b35cf9fb3
RyogaKobayashi <ryoga0629.k@icloud.com>
2021-04-20 17:56:47 +0900

    420commit

---
+++ chap2.rb
@@ -0,0 +1,11 @@
+describe 'should be able to login' do 
+  let(:user) { FactoryGirl.creat(:user) }
+  before do 
+    visit login_path
+    fill_in 'Email', with: user.email 
+    fill_in 'Password', with: user.password 
+    click_button 'Sign in'
+  end
+  it { should have_selector('h1', text: 'Welcome')}
+end
+
---
+++ chap3.rb
@@ -0,0 +1,48 @@
+#失敗例
+describe 'When creating a new user' do 
+  subject { page } 
+  describe 'with valid credentials' do 
+    before do 
+      visit signup_path
+      fill_in 'Name', with: 'New User'
+      fill_in 'Email', with: 'user@example.com'
+      fill_in 'Password', with: 'foobar'
+      fill_in 'Confirmation', with: 'foobar'
+      click_button 'Create my account'
+    end
+    describe 'after saving the user' do 
+      let(:user) { User.find_by(email: 'user@example.com') }
+
+      it { should have_content(user.name) }
+      it { should have_selector('.alert-success' ) }
+    end
+  end
+end
+
+
+describe 'When creating a new user' do 
+  subject { page } 
+  describe 'with valid credentials' do 
+    before do 
+      visit signup_path
+      fill_in 'Name', with: 'New User'
+      fill_in 'Email', with: 'user@example.com'
+      fill_in 'Password', with: 'foobar'
+      fill_in 'Confirmation', with: 'foobar'
+      click_button 'Create my account'
+    end
+    describe 'after saving the user' do 
+      let(:user) { User.find_by(email: 'user@example.com') }
+
+      it { should have_title(user.name) }
+      it { should have_selector('.alert-success') }
+    end
+  end
+  describe 'with invalid credentials' do 
+    before do 
+      visit signup_path
+      click_button 'Creat my account'
+    end
+    it { should have_selector('.alert-account') }
+  end
+end
